23bce91dc9b32d7805c99e380abbacf763651643
Cdorey <Bsense@live.com>
2018-10-20 19:53:09 +0800

    push

---
+++ FlyingAria2c/DownloadTask.cs
@@ -0,0 +1,12 @@
+﻿namespace FlyingAria2c
+{
+    public class DownloadTask
+    {
+
+
+        public static DownloadTask Create(string DownloadAdress)
+        {
+            throw new System.Exception();
+        }
+    }
+}
---
+++ FlyingAria2c/Downloader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlyingAria2c
+{
+    public static class Downloader
+    {
+        private static Queue<DownloadTask> DownloadingQueue { get; set; }
+
+    }
+}
