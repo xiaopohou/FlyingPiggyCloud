3a20a2fd0437ee8c77f84b230c27eaf5c127850b
Cdorey <Bsense@live.com>
2019-02-23 15:19:13 +0800

    LoginViewDesignFinished

--- LiuPan/Views/LoginView.xaml.cs
+++ LiuPan/Views/LoginView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SixCloudCustomControlLibrary.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace SixCloud.Views
     /// <summary>
     /// LoginView.xaml 的交互逻辑
     /// </summary>
-    public partial class LoginView : Window
+    public partial class LoginView : MetroWindow
     {
         public LoginView()
         {
--- SixCloudCustomControlLibrary/Controls/MetroWindow.cs
+++ SixCloudCustomControlLibrary/Controls/MetroWindow.cs
@@ -8,27 +8,16 @@ namespace SixCloudCustomControlLibrary.Controls
     public class MetroWindow : Window
     {
         public static readonly DependencyProperty IsSubWindowShowProperty = DependencyProperty.Register("IsSubWindowShow", typeof(bool), typeof(MetroWindow), new PropertyMetadata(false));
-        public static readonly DependencyProperty MenuProperty = DependencyProperty.Register("Menu", typeof(object), typeof(MetroWindow), new PropertyMetadata(null));
+        //public static readonly DependencyProperty MenuProperty = DependencyProperty.Register("Menu", typeof(object), typeof(MetroWindow), new PropertyMetadata(null));
         public static new readonly DependencyProperty BorderBrushProperty = DependencyProperty.Register("BorderBrush", typeof(Brush), typeof(MetroWindow));
         public static readonly DependencyProperty TitleForegroundProperty = DependencyProperty.Register("TitleForeground", typeof(Brush), typeof(MetroWindow));
+        public static readonly DependencyProperty MetroWindowLayoutProperty = DependencyProperty.Register("MetroWindowLayout", typeof(MetroWindowLayout), typeof(MetroWindow), new PropertyMetadata(MetroWindowLayout.Normal));
 
         public bool IsSubWindowShow { get => (bool)GetValue(IsSubWindowShowProperty); set => SetValue(IsSubWindowShowProperty, value); }
-        public object Menu { get => GetValue(MenuProperty); set => SetValue(MenuProperty, value); }
+        //public object Menu { get => GetValue(MenuProperty); set => SetValue(MenuProperty, value); }
         public new Brush BorderBrush { get => (Brush)GetValue(BorderBrushProperty); set => SetValue(BorderBrushProperty, value); }
         public Brush TitleForeground { get => (Brush)GetValue(TitleForegroundProperty); set => SetValue(TitleForegroundProperty, value); }
-
-        //private void BorderBrushChange(Brush brush)
-        //{
-        //    if (IsLoaded)
-        //    {
-        //        Theme.Switch(this);
-        //    }
-        //}
-
-        //private void GoToState()
-        //{
-        //    ElementBase.GoToState(this, IsSubWindowShow ? "Enabled" : "Disable");
-        //}
+        public MetroWindowLayout MetroWindowLayout { get => (MetroWindowLayout)GetValue(MetroWindowLayoutProperty); set => SetValue(MetroWindowLayoutProperty, value); }
 
         public object ReturnValue { get; set; } = null;
         public bool EscClose { get; set; } = false;
@@ -82,8 +71,13 @@ namespace SixCloudCustomControlLibrary.Controls
 
         static MetroWindow()
         {
-            //ElementBase.DefaultStyle<MetroWindow>(DefaultStyleKeyProperty);
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MetroWindow), new FrameworkPropertyMetadata(typeof(MetroWindow)));
         }
+    }
+
+    public enum MetroWindowLayout
+    {
+        Normal,
+        WideAngle
     }
 }
