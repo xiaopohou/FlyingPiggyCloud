48acdef74d5b624062a72b5f19456a05b608cdee
Cdorey <Bsense@live.com>
2019-03-14 21:11:26 +0800

    push

---
+++ LiuPan/Views/UserControls/RenamableTextBox.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SixCloud.Views.UserControls
+{
+    /// <summary>
+    /// RenamableTextBox.xaml 的交互逻辑
+    /// </summary>
+    public partial class RenamableTextBox : UserControl
+    {
+        public static readonly DependencyProperty CurrentNameProperty = DependencyProperty.Register("CurrentName", typeof(string), typeof(RenamableTextBox));
+
+        public string CurrentName { get => (string)GetValue(CurrentNameProperty); set => SetValue(CurrentNameProperty, value); }
+
+        public static readonly DependencyProperty RenameProperty = DependencyProperty.Register("Rename", typeof(bool), typeof(RenamableTextBox));
+
+        public bool Rename { get => (bool)GetValue(RenameProperty); set => SetValue(RenameProperty, value); }
+
+        public RenamableTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
