73557351a96df4a7c6bfa18cff1cb1b4fc849541
Cdorey <Bsense@live.com>
2019-03-17 15:19:48 +0800

    push

--- LiuPan/ViewModels/FileListItemViewModel.cs
+++ LiuPan/ViewModels/FileListItemViewModel.cs
@@ -150,11 +150,80 @@ namespace SixCloud.ViewModels
         {
             IconDictionary = new Dictionary<string, string>
             {
-
+                {"default",'\uf15c'.ToString() },
+                {"folder",'\uf07b'.ToString() },
+                {".zip",'\uf1c6'.ToString() },
+                {".rar",'\uf1c6'.ToString() },
+                {".7z",'\uf1c6'.ToString() },
+                {".tar",'\uf1c6'.ToString() },
+                {".gz",'\uf1c6'.ToString() },
+                {".iso",'\uf1c6'.ToString() },
+                {".dmg",'\uf1c6'.ToString() },
+                {".img",'\uf1c6'.ToString() },
+                {".mp3",'\uf1c7'.ToString() },
+                {".wma",'\uf1c7'.ToString() },
+                {".wav",'\uf1c7'.ToString() },
+                {".ape",'\uf1c7'.ToString() },
+                {".flac",'\uf1c7'.ToString() },
+                {".ogg",'\uf1c7'.ToString() },
+                {".aac",'\uf1c7'.ToString() },
+                {".cs",'\uf1c9'.ToString() },
+                {".css",'\uf1c9'.ToString() },
+                {".html",'\uf1c9'.ToString() },
+                {".js",'\uf1c9'.ToString() },
+                {".ts",'\uf1c9'.ToString() },
+                {".cc",'\uf1c9'.ToString() },
+                {".h",'\uf1c9'.ToString() },
+                {".c",'\uf1c9'.ToString() },
+                {".hpp",'\uf1c9'.ToString() },
+                {".hxx",'\uf1c9'.ToString() },
+                {".cpp",'\uf1c9'.ToString() },
+                {".cxx",'\uf1c9'.ToString() },
+                {".xaml",'\uf1c9'.ToString() },
+                {".php",'\uf1c9'.ToString() },
+                {".jsp",'\uf1c9'.ToString() },
+                {".jar",'\uf1c9'.ToString() },
+                {".java",'\uf1c9'.ToString() },
+                {".asp",'\uf1c9'.ToString() },
+                {".aspx",'\uf1c9'.ToString() },
+                {".class",'\uf1c9'.ToString() },
+                {".go",'\uf1c9'.ToString() },
+                {".xls",'\uf1c3'.ToString() },
+                {".xlsx",'\uf1c3'.ToString() },
+                {".xlsb",'\uf1c3'.ToString() },
+                {".xlsm",'\uf1c3'.ToString() },
+                {".csv",'\uf1c3'.ToString() },
+                {".jpg",'\uf1c5'.ToString() },
+                {".png",'\uf1c5'.ToString() },
+                {".bmp",'\uf1c5'.ToString() },
+                {".gif",'\uf1c5'.ToString() },
+                {".tif",'\uf1c5'.ToString() },
+                {".swf",'\uf1c5'.ToString() },
+                {".ico",'\uf1c5'.ToString() },
+                {".jpeg",'\uf1c5'.ToString() },
+                {".pdf",'\uf1c1'.ToString() },
+                {".ppt",'\uf1c4'.ToString() },
+                {".pptx",'\uf1c4'.ToString() },
+                {".pptm",'\uf1c4'.ToString() },
+                {".ppsx",'\uf1c4'.ToString() },
+                {".pps",'\uf1c4'.ToString() },
+                {".avi",'\uf1c8'.ToString() },
+                {".mp4",'\uf1c8'.ToString() },
+                {".f4v",'\uf1c8'.ToString() },
+                {".m4v",'\uf1c8'.ToString() },
+                {".rmvb",'\uf1c8'.ToString() },
+                {".mkv",'\uf1c8'.ToString() },
+                {".mpg",'\uf1c8'.ToString() },
+                {".mov",'\uf1c8'.ToString() },
+                {".wmv",'\uf1c8'.ToString() },
+                {".mpe",'\uf1c8'.ToString() },
+                {".mpeg",'\uf1c8'.ToString() },
+                {".doc",'\uf1c2'.ToString() },
+                {".docx",'\uf1c2'.ToString() },
             };
         }
 
-        public FileListItemViewModel(FileListViewModel parent,FileMetaData fileMetaData)
+        public FileListItemViewModel(FileListViewModel parent, FileMetaData fileMetaData)
         {
             Parent = parent;
             Name = fileMetaData.Name;
@@ -174,6 +243,16 @@ namespace SixCloud.ViewModels
             ConfirmCommand = new DependencyCommand(Confirm, CanConfirm);
             DownloadCommand = new DependencyCommand(Download, CanDownload);
             MoreCommand = new DependencyCommand(More, AlwaysCan);
+
+            if (Type == 0)
+            {
+                string eName = System.IO.Path.GetExtension(Name);
+                Icon = IconDictionary.ContainsKey(eName) ? IconDictionary[eName] : IconDictionary["default"];
+            }
+            else
+            {
+                Icon = IconDictionary["folder"];
+            }
         }
     }
 }
