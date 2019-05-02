1ec84393928f0ced0ce29a0a734163b90dbdfc0d
Cdorey <Bsense@live.com>
2019-05-03 01:09:56 +0800

    push

---
+++ SixCloudCustomControlLibrary/Controls/LightTabControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SixCloudCustomControlLibrary.Controls
+{
+    public class LightTabControl : TabControl
+    {
+        static LightTabControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LightTabControl), new FrameworkPropertyMetadata(typeof(LightTabControl)));
+        }
+    }
+
+    public class LightTabItem : TabItem
+    {
+        public static readonly DependencyProperty FontIconProperty = DependencyProperty.Register("FontIcon", typeof(string), typeof(LightTabItem), new PropertyMetadata('\uf111'.ToString()));
+
+        public string FontIcon { get => (string)GetValue(FontIconProperty); set => SetValue(FontIconProperty, value); }
+
+        static LightTabItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LightTabItem), new FrameworkPropertyMetadata(typeof(LightTabItem)));
+        }
+    }
+}
---
+++ SixCloudCustomControlLibrary/Controls/MetroFocusButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace SixCloudCustomControlLibrary.Controls
+{
+    public class MetroFocusButton : Button
+    {
+        public static readonly DependencyProperty MouseMoveForegroundProperty = ElementBase.Property<MetroFocusButton, Brush>(nameof(MouseMoveForegroundProperty));
+        public static readonly DependencyProperty MouseMoveBorderBrushProperty = ElementBase.Property<MetroFocusButton, Brush>(nameof(MouseMoveBorderBrushProperty));
+        public static new readonly DependencyProperty BorderThicknessProperty = ElementBase.Property<MetroFocusButton, double>(nameof(BorderThicknessProperty));
+        public static readonly DependencyProperty MouseMoveBorderThicknessProperty = ElementBase.Property<MetroFocusButton, double>(nameof(MouseMoveBorderThicknessProperty));
+        public static readonly DependencyProperty StrokeDashArrayProperty = ElementBase.Property<MetroFocusButton, DoubleCollection>(nameof(StrokeDashArrayProperty));
+        public static readonly DependencyProperty MouseMoveStrokeDashArrayProperty = ElementBase.Property<MetroFocusButton, DoubleCollection>(nameof(MouseMoveStrokeDashArrayProperty));
+        public static readonly DependencyProperty CornerRadiusProperty = ElementBase.Property<MetroFocusButton, double>(nameof(CornerRadiusProperty));
+
+        public Brush MouseMoveForeground { get => (Brush)GetValue(MouseMoveForegroundProperty); set => SetValue(MouseMoveForegroundProperty, value); }
+        public Brush MouseMoveBorderBrush { get => (Brush)GetValue(MouseMoveBorderBrushProperty); set => SetValue(MouseMoveBorderBrushProperty, value); }
+        public new double BorderThickness { get => (double)GetValue(BorderThicknessProperty); set => SetValue(BorderThicknessProperty, value); }
+        public double MouseMoveBorderThickness { get => (double)GetValue(MouseMoveBorderThicknessProperty); set => SetValue(MouseMoveBorderThicknessProperty, value); }
+        public DoubleCollection StrokeDashArray { get => (DoubleCollection)GetValue(StrokeDashArrayProperty); set => SetValue(StrokeDashArrayProperty, value); }
+        public DoubleCollection MouseMoveStrokeDashArray { get => (DoubleCollection)GetValue(MouseMoveStrokeDashArrayProperty); set => SetValue(MouseMoveStrokeDashArrayProperty, value); }
+        public double CornerRadius { get => (double)GetValue(CornerRadiusProperty); set => SetValue(CornerRadiusProperty, value); }
+
+        static MetroFocusButton()
+        {
+            ElementBase.DefaultStyle<MetroFocusButton>(DefaultStyleKeyProperty);
+        }
+    }
+}
