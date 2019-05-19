36cf7685022663b8a9ae5df472e51ef8400be9aa
Cdorey <Bsense@live.com>
2019-05-19 13:29:31 +0800

    push

--- LiuPan/Models/OfflineTaskParseUrl.cs
+++ LiuPan/Models/OfflineTaskParseUrl.cs
@@ -20,31 +20,4 @@ namespace SixCloud.Models
         [JsonProperty(PropertyName = "files")]
         public OfflineTaskParseFile[] Files { get; set; }
     }
-
-    internal class OfflineTaskAdd
-    {
-        [JsonProperty(PropertyName = "success")]
-        public bool Success { get; set; }
-
-        [JsonProperty(PropertyName = "count")]
-        public int Count { get; set; }
-    }
-
-    internal class OfflineTaskParameters
-    {
-        public OfflineTaskParameters(string identity, string[] iginreFiles=null)
-        {
-            Identity = identity;
-            IginreFiles = iginreFiles;
-        }
-
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "identity")]
-        public string Identity { get; set; }
-
-        /// <summary>
-        /// 所在文件的 PathIdentity
-        /// </summary>
-        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "iginreFiles")]
-        public string[] IginreFiles { get; set; }
-    }
 }
--- LiuPan/Views/OfflineUrlsDialog.xaml.cs
+++ LiuPan/Views/OfflineUrlsDialog.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using SixCloudCustomControlLibrary.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace SixCloud.Views
 {
@@ -23,6 +11,15 @@ namespace SixCloud.Views
         public OfflineUrlsDialog()
         {
             InitializeComponent();
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                System.Threading.Thread.Sleep(1000);
+                App.Current.Dispatcher.Invoke(() =>
+                {
+                    IsInputUrlsStage.IsChecked = true;
+                });
+            });
+
         }
 
         private void CutDownRadioButtonChecked(object sender, RoutedEventArgs e)
