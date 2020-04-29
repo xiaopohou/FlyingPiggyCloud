86eb41f526b98e0df0ccd58cd31d9846d928fb24
Cdorey <Bsense@live.com>
2020-04-29 21:23:53 +0800

    重做传输任务列表

---
+++ SixCloudCore/ViewModels/ITransferItemViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace SixCloudCore.ViewModels
+{
+    public interface ITransferItemViewModel
+    {
+        public string Icon { get; set; }
+
+        public string Name { get; set; }
+
+        public double Progress { get; }
+
+        public string Status { get; set; }
+
+        public string Completed { get; set; }
+
+        public string Total { get; set; }
+
+        public string Speed { get; set; }
+
+        public DependencyCommand RecoveryCommand { get; set; }
+
+        public DependencyCommand PauseCommand { get; set; }
+
+        public DependencyCommand CancelCommand { get; set; }
+    }
+}
