7805f66a2d52e06d7bf36ddc1cc9f0153d82a587
Cdorey <Bsense@live.com>
2020-05-14 23:26:01 +0800

    VLC示例代码

--- SixCloudCore/Views/VLCView.xaml.cs
+++ SixCloudCore/Views/VLCView.xaml.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LibVLCSharp.Shared;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using MediaPlayer = LibVLCSharp.Shared.MediaPlayer;
 
 namespace SixCloudCore.Views
 {
@@ -17,10 +10,30 @@ namespace SixCloudCore.Views
     /// </summary>
     public partial class VLCView : Window
     {
+        private readonly LibVLC _libVLC;
+        private readonly MediaPlayer _mediaPlayer;
+
+        protected override void OnClosed(EventArgs e)
+        {
+            MainContainer.Dispose();
+        }
+
         public VLCView()
         {
             InitializeComponent();
-            MainContainer.
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            // we need the VideoView to be fully loaded before setting a MediaPlayer on it.
+            MainContainer.Loaded += (sender, e) =>
+            {
+                MainContainer.MediaPlayer = _mediaPlayer;
+                MainContainer.MediaPlayer.Play(new Media(_libVLC, "http://hslSource", FromType.FromLocation));
+            };
+
+
+
         }
     }
 }
