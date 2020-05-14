acad89a16d552b6d12a4a5ee553a88843082f3dd
Cdorey <Bsense@live.com>
2020-05-14 20:15:13 +0800

    引入CefSharp和LibVLCSharp

--- SixCloudCore/App.xaml.cs
+++ SixCloudCore/App.xaml.cs
@@ -1,5 +1,12 @@
-﻿using Sentry;
+﻿using CefSharp;
+using CefSharp.Wpf;
+using LibVLCSharp.Shared;
+using Sentry;
 using SixCloudCore.ViewModels;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace SixCloudCore
@@ -11,9 +18,41 @@ namespace SixCloudCore
     {
         public App()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += Resolver;
+            InitializeCefSharp();
             SentrySdk.Init("https://aa9303eba050450187a9c04653e74be5@o387540.ingest.sentry.io/5222970");
+            Core.Initialize();
             new LoginWebViewModel();
             //new MainWindow().Show();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void InitializeCefSharp()
+        {
+            var settings = new CefSettings();
+
+            // Set BrowserSubProcessPath based on app bitness at runtime
+            settings.BrowserSubprocessPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                                                   Environment.Is64BitProcess ? "x64" : "x86",
+                                                   "CefSharp.BrowserSubprocess.exe");
+
+            // Make sure you set performDependencyCheck false
+            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
+        }
+
+        // Will attempt to load missing assembly from either x86 or x64 subdir
+        // Required by CefSharp to load the unmanaged dependencies when running using AnyCPU
+        private static Assembly Resolver(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.StartsWith("CefSharp"))
+            {
+                string assemblyName = args.Name.Split(new[] { ',' }, 2)[0] + ".dll";
+                string archSpecificPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, Environment.Is64BitProcess ? "x64" : "x86", assemblyName);
+
+                return File.Exists(archSpecificPath) ? Assembly.LoadFile(archSpecificPath) : null;
+            }
+
+            return null;
         }
     }
 }
